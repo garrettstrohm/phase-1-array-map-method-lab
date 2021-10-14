b664718358b7363cf7b28f1d95ec46369114e0b6
Garrett <garres4@vt.edu>
2021-10-14 11:52:54 -0600

    commit tree d89864113c334016fa9407e5fa74bd55b55167b2

--- index.js
+++ index.js
@@ -11,6 +11,61 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(titles => {
+  let deconstructedTitles = titles.split(" ");
+  const capitalizedTitles = deconstructedTitles.map(title => {
+    return title[0].toUpperCase() + title.substr(1);
+  }).join(" ");
+  return capitalizedTitles;
+}) 
+
+
+
+// const words = testTutorial.split(" ");
+
+// const capitalizedTutorial = words.map(word => { 
+//   return word[0].toUpperCase() + word.substring(1);
+// }).join(" ");
+// console.log(capitalizedTutorial)
+// const students = ["harry", "ron", "hermione", "ginevra"];
+// const rollCall = [];
+
+// for (const student of students) {
+//   rollCall.push( student + " the wizard" );
+// }
+
+// function studentRollCall(student) {
+//   return student + " the wizard";
+// }
+
+// const students = ["harry", "ron", "hermione", "ginevra"];
+// const rollCall = students.map(studentRollCall);
+// console.log(rollCall);
+
+// const students = ["harry", "ron", "hermione", "ginevra"];
+// const rollCall = students.map(function(students) {
+//   return students + " the wizard";
+// })
+
+// console.log(rollCall);
+
+// const students = ["harry", "ron", "hermione", "ginevra"];
+// const rollCall = students.map(students => students + " the wizard");
+
+// console.log(rollCall);
+
+// const robots = [
+//   { name: 'Johnny 5', modes: 5, isActivated: false, },
+//   { name: 'C3PO', modes: 3, isActivated: false, },
+//   { name: 'Sonny', modes: 2.5, isActivated: false, },
+//   { name: 'Baymax', modes: 1.5, isActivated: false, },
+// ];
+
+// const activatedRobots = robots.map(robot => {
+//   return Object.assign({}, robot, {
+//     modes: robot.modes * 2,
+//     isActivated: true,
+//   });
+// });
+
+// console.log(activatedRobots);
